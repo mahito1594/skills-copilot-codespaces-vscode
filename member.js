6452ec86d30d199d47988c0fd31680b3368c25ce
Mahito TANNO <pb94.mahito@gmail.com>
2024-03-17 07:23:12 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JavaScript"];
+    var member = {
+        name: "John",
+        age: 25,
+        skills: skills
+    };
+    return member;
+}
